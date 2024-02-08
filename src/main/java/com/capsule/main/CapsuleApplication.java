a449fe13d86b756844e0933afd860f376af1a431
subeen0210 <subeen0210@gmail.com>
2024-02-08 17:18:34 +0900

    first

---
+++ src/main/java/com/capsule/main/CapsuleApplication.java
@@ -0,0 +1,13 @@
+package com.capsule.main;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CapsuleApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CapsuleApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/capsule/main/CapsuleApplicationTests.java
@@ -0,0 +1,13 @@
+package com.capsule.main;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CapsuleApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
