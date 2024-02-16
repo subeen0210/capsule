9041c0e319e26dac30ab2df860117bf9c60c5f68
orchid_k <kuran1286@gmail.com>
2024-02-16 16:34:03 +0900

    [rh] index arrow 변경

--- src/main/java/com/capsule/main/login/LoginService.java
+++ src/main/java/com/capsule/main/login/LoginService.java
@@ -2,7 +2,6 @@ package com.capsule.main.login;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
-import org.springframework.ui.Model;
 
 import jakarta.servlet.http.HttpSession;
 
