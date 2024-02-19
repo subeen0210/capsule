59b90530516021b1864d2eee6d28c3cdf1791124
subeen0210 <subeen0210@gmail.com>
2024-02-19 12:39:58 +0900

    모달창 jstl 추가 css border 부분 주석처리 HomeC session 추가

--- src/main/java/com/capsule/main/home/HomeC.java
+++ src/main/java/com/capsule/main/home/HomeC.java
@@ -5,13 +5,16 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+import jakarta.servlet.http.HttpSession;
+
 @RequestMapping("/happy-capsule")
 @Controller
 public class HomeC {
 
 	
 	@GetMapping("/main")
-	public String makeMemoGO(Model model) {
+	public String makeMemoGO(Model model, HttpSession hs) {
+		System.out.println(hs.getAttribute("user"));
 		model.addAttribute("content", "/WEB-INF/views/home/homeContent.jsp");
 		return "home";
 	}
