c693649c57a17d6075f8553d98858e3f1da9f66d
allelouia <allelouia1574@gmail.com>
2024-02-19 17:53:10 +0900

    shelf animation effect fix

--- src/main/resources/static/js/mg_shelf.js
+++ src/main/resources/static/js/mg_shelf.js
@@ -4,17 +4,10 @@ let shelfLeft = '';
 let shelfRight = '';
 
 document.addEventListener("DOMContentLoaded", function() {
-	document.querySelectorAll('.shelf').forEach(function(e) {
-		e.style.transition = 'all 3s ease-in-out';
-	})
-	
+
 	shelfLeft = document.querySelector('.shelf-paging-1').getBoundingClientRect().left;
 	shelfRight = document.querySelector('.shelf-paging-1').getBoundingClientRect().right;
-	shelfTop = document.querySelector('.shelf-paging-1').getBoundingClientRect().top;
-
-	console.log(shelfLeft)
-	console.log(shelfRight)
-
+	document.querySelector('.shelf-paging-1').style.left = shelfLeft + 'px';
 
 	if (document.querySelector('.list-content').lastElementChild.classList[1].slice(-1) == 1) {
 		document.querySelectorAll('.go-icon').forEach(function(e) {
@@ -23,20 +16,12 @@ document.addEventListener("DOMContentLoaded", function() {
 	} else {
 		lastPage = document.querySelector('.list-content').lastElementChild.classList[1].slice(-1);
 
-		document.querySelectorAll('.added-shelf').forEach(function(e) {
-			e.style.display = 'none';
-			e.style.left = '100vw';
-		})
-
-
 		if (currentPage == 1) {
 			document.querySelectorAll('.go-icon')[0].style.display = 'none';
 		}
 
 		document.querySelectorAll('.go-icon')[0].addEventListener("click", function() {
 			document.querySelector('.shelf-paging-' + currentPage).style.left = '100vw';
-			document.querySelector('.shelf-paging-' + currentPage).style.display = 'none';
-			document.querySelector('.shelf-paging-' + (currentPage - 1)).style.display = '';
 			document.querySelector('.shelf-paging-' + (currentPage - 1)).style.left = shelfLeft + 'px';
 
 
@@ -52,11 +37,8 @@ document.addEventListener("DOMContentLoaded", function() {
 		})
 
 		document.querySelectorAll('.go-icon')[1].addEventListener("click", function() {
-			console.log(currentPage)
-			document.querySelector('.shelf-paging-' + currentPage).style.right = '100vw';
-			document.querySelector('.shelf-paging-' + currentPage).style.display = 'none';
-			document.querySelector('.shelf-paging-' + (currentPage + 1)).style.display = '';
-			document.querySelector('.shelf-paging-' + (currentPage + 1)).style.left = '';
+			document.querySelector('.shelf-paging-' + currentPage).style.left = '-' + shelfRight + 'px';
+			document.querySelector('.shelf-paging-' + (currentPage + 1)).style.left = shelfLeft + 'px';
 
 			currentPage += 1;
 
