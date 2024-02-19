3f1a409ed17576754e39f2b3136e96e1e6c8e3df
allelouia <allelouia1574@gmail.com>
2024-02-19 12:09:31 +0900

    shelf login fix

--- src/main/resources/static/js/mg_shelf.js
+++ src/main/resources/static/js/mg_shelf.js
@@ -13,12 +13,10 @@ document.addEventListener("DOMContentLoaded", function() {
 		document.querySelectorAll('.go-icon').forEach(function(e) {
 			e.style.display = 'none';
 		})
-
 	} else {
 		document.querySelectorAll('.added-shelf').forEach(function(e) {
 			e.style.left = '100vw';
 		})
-
 		lastPage = document.querySelector('.list-content').lastElementChild.classList[1].slice(-1);
 
 		if (currentPage == 1) {
@@ -37,7 +35,6 @@ document.addEventListener("DOMContentLoaded", function() {
 				document.querySelectorAll('.go-icon')[1].style.display = 'flex';
 			}
 		})
-
 		document.querySelectorAll('.go-icon')[1].addEventListener("click", function() {
 			console.log(currentPage)
 			document.querySelector('.shelf-paging-' + currentPage).style.left = '-' + (shelfRight - shelfLeft) + 'px';
@@ -53,53 +50,3 @@ document.addEventListener("DOMContentLoaded", function() {
 		})
 	}
 });
-
-function shelfList(id) {
-	console.log(id.value)
-
-	var form = document.createElement('form');
-	form.method = 'post'; // POST 방식 설정
-	form.action = '/happy-capsule/shelf/list'; // 서블릿의 URL로 설정
-
-	// 입력 필드 생성 및 설정
-	var input1 = document.createElement('input');
-	input1.name = 'id'; // 서블릿에서 읽을 파라미터 이름
-	input1.value = id.value; // 전송할 값
-
-	// 폼에 입력 필드 추가
-	form.appendChild(input1);
-
-	// 폼을 body에 추가하고 자동으로 제출
-	document.body.appendChild(form);
-	form.submit();
-}
-
-function shelfListJson(id) {
-	fetch('/happy-capsule/shelf/list/' + id.value, {
-		method: "POST"
-	})
-		.then(response => response.json())
-		.then(data => {
-			console.log(data)
-		})
-		.catch(error => {
-			console.error('데이터를 가져오는 중 오류 발생:', error);
-		});
-
-	//	var form = document.createElement('form');
-	//	form.method = 'get'; // POST 방식 설정
-	//	form.action = '/happy-capsule/shelf/listjson'; // 서블릿의 URL로 설정
-	//
-	//	// 입력 필드 생성 및 설정
-	//	var input1 = document.createElement('input');
-	//	input1.name = 'id'; // 서블릿에서 읽을 파라미터 이름
-	//	input1.value = id.value; // 전송할 값
-	//
-	//	// 폼에 입력 필드 추가
-	//	form.appendChild(input1);
-	//
-	//	// 폼을 body에 추가하고 자동으로 제출
-	//	document.body.appendChild(form);
-	//	form.submit();
-}
-
