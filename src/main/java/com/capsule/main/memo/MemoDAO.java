a79c16bc66e4d9e64a0374d4d4e82b6ffb06973c
subeen0210 <subeen0210@gmail.com>
2024-02-27 17:22:11 +0900

    MemoDTO 메모 업로드 기능 수정

--- src/main/java/com/capsule/main/memo/MemoDAO.java
+++ src/main/java/com/capsule/main/memo/MemoDAO.java
@@ -41,72 +41,71 @@ public class MemoDAO {
 		return mMapper.openMemo(no);
 	}
 
+	
 	public int insertMemo(MemoDTO memoDTO, HttpSession hs) {
-		try {
-			System.out.println(memoDTO);
-//			String fileName = memoDTO.getM_file().getOriginalFilename();
+	    try {
+	        System.out.println(memoDTO);
+	        
+	        // 파일이 null이 아닌 경우에만 파일 업로드 처리 수행
+	        if (memoDTO.getM_file() != null) {
+	            FileInputStream serviceAccount = new FileInputStream("src/main/resources/serviceKey.json");
 
-			FileInputStream serviceAccount = new FileInputStream("src/main/resources/serviceKey.json");
+	            if (FirebaseApp.getApps().isEmpty()) {
+	                FirebaseOptions options = FirebaseOptions.builder()
+	                        .setCredentials(GoogleCredentials.fromStream(serviceAccount)).build();
+	                FirebaseApp.initializeApp(options);
+	            }
 
-//			int data;
-//			while ((data = serviceAccount.read()) != -1 ) {
-//				System.out.print((char)data);
-//			}
-			if (FirebaseApp.getApps().isEmpty()) {
-				FirebaseOptions options = FirebaseOptions.builder()
-						.setCredentials(GoogleCredentials.fromStream(serviceAccount)).build();
-				FirebaseApp.initializeApp(options);
-			}
+	            // Firebase Storage 인스턴스 가져오기
+	            Storage storage = StorageOptions.newBuilder().setCredentials(GoogleCredentials.fromStream(serviceAccount))
+	                    .build().getService();
 
-			// Firebase Storage 인스턴스 가져오기
-			Storage storage = StorageOptions.newBuilder().setCredentials(GoogleCredentials.fromStream(serviceAccount))
-					.build().getService();
+	            String saveFileName = UUID.randomUUID().toString().split("-")[0];
+	            System.out.println(saveFileName);
+	            // 업로드할 파일 경로 및 이름 설정
+	            String bucketName = "happybottle-13b47.appspot.com";
+	            String objectName = "memory/" + saveFileName;
 
-			String saveFileName = UUID.randomUUID().toString().split("-")[0];
-			System.out.println(saveFileName);
-			// 업로드할 파일 경로 및 이름 설정
-			String bucketName = "happybottle-13b47.appspot.com";
-			String objectName = "memory/" + saveFileName;
+	            // 업로드할 파일 객체 생성
+	            BlobId blobId = BlobId.of(bucketName, objectName);
+	            BlobInfo blobInfo = BlobInfo.newBuilder(blobId).setContentType("image/jpeg").build();
 
-			// 업로드할 파일 객체 생성
-			BlobId blobId = BlobId.of(bucketName, objectName);
-			BlobInfo blobInfo = BlobInfo.newBuilder(blobId).setContentType("image/jpeg").build();
+	            // 파일을 Firebase Storage에 업로드
+	            Blob blob = storage.create(blobInfo, memoDTO.getM_file().getBytes());
 
-			// 파일을 Firebase Storage에 업로드
-			Blob blob = storage.create(blobInfo, memoDTO.getM_file().getBytes());
+	            System.out.println("File " + saveFileName + " uploaded successfully.");
 
-			System.out.println("File " + saveFileName + " uploaded successfully.");
+	            // 업로드한 파일의 Blob 객체에서 공개적인 URL 가져오기
+	            String publicUrl = "https://firebasestorage.googleapis.com/v0/b/" + bucketName + "/o/memory%2F"
+	                    + saveFileName + "?alt=media";
 
-			// 업로드한 파일의 Blob 객체에서 공개적인 URL 가져오기
-			String publicUrl = "https://firebasestorage.googleapis.com/v0/b/" + bucketName + "/o/memory%2F"
-					+ saveFileName + "?alt=media";
+	            // 저장할 이미지 경로 - 요걸 m_pic에 insert하면 됨
+	            System.out.println("Public URL: " + publicUrl);
+	            
+	            memoDTO.setM_pic(publicUrl);
+	        } else {
+	            // 파일이 없는 경우 null 또는 다른 기본값으로 설정
+	            memoDTO.setM_pic(null);
+	        }
+	        
+	        if(memoDTO.getB_no()==0) {
+	            UserDTO userDTO = (UserDTO) hs.getAttribute("user");
+	            if(bMapper.createBottle(userDTO.getU_id())==1) {
+	                bottlePk = bMapper.curBottleNo();
+	                memoDTO.setB_no(bottlePk);
+	            }
+	        } else if (memoDTO.getB_no() != 0) {
+	            
+	        }
+	        
+	        mMapper.insertMemo(memoDTO);
+	        System.out.println("================ pk : " + bottlePk);
+	        return bottlePk;
 
-			// 저장할 이미지 경로 - 요걸 m_pic에 insert하면 됨
-			System.out.println("Public URL: " + publicUrl);
-			
-			memoDTO.setM_pic(publicUrl);
-			if(memoDTO.getB_no()==0) {
-				UserDTO userDTO = (UserDTO) hs.getAttribute("user");
-				if(bMapper.createBottle(userDTO.getU_id())==1) {
-					bottlePk = bMapper.curBottleNo();
-					memoDTO.setB_no(bottlePk);
-				}
-			} else if (memoDTO.getB_no() != 0) {
-				
-			}
-			
-			mMapper.insertMemo(memoDTO);
-			System.out.println("================ pk : " + bottlePk);
-			return bottlePk;
-			
-			
-			
-			
-
-		} catch (Exception e) {
-			e.printStackTrace();
-		}
-		return 0;
+	    } catch (Exception e) {
+	        e.printStackTrace();
+	    }
+	    return 0;
 	}
 
 	
